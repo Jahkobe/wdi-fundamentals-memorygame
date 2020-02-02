dec96cd8623077f346301eeb316ac2eef2d857e2
Jahkobe <jpursell264@gmail.com>
2020-02-01 19:31:56 -0700

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,7 +1,24 @@
-console.log("Up and running!");
-let first = "Queen";
-let second = "Queen";
-let third = "King";
-let fourth = "King";
 
-console.log("User flipped " + first);
+
+alert('Hello, friends.');
+
+const cards = ["queen", "queen", "king" ,"king"];
+const cardsInPlay =[];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped queen");
+var cardTwo = [2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped king");
+
+if(cardsInPlay.length === 2){
+
+if(cardsInPlay[0]===cardsInPlay[1]){
+alert("You found a match!");
+}
+else{
+	alert("Sorry, try again");
+}}
+
+
+
